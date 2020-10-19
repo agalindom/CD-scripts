40cbcad02d58388bb9f0c8b0d4272dc2803755bb
Alejandro Galindo <alegmed09@gmail.com>
2020-10-18 21:04:43 -0500

    Updated files

--- ansible-inventory/setup/files/index.js
+++
@@ -1,6 +0,0 @@
-var http = require("../../../.circleci/ansible/roles/setup/files/node_modules/http");
-var server = http.createServer(function (req, res) {
-res.writeHead(200);
-res.end("Hello world!");
-});
-server.listen(3000);
